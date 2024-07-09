940e3e59a495f65cf5546efafa3ab0e6b702166b
shyam2511 <nvshyamfacebook@gmail.com>
2024-07-09 17:22:48 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './Components/Header'
+import Sidebar from './Components/Sidebar'
+import Home from './Components/Home'
+import Footer from './Components/Footer'
+import './App.css'
+import { useState } from 'react'
+
 
 function App() {
+  const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
+
+  const OpenSidebar = () => {
+    setOpenSidebarToggle(!openSidebarToggle)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='grid-container'>
+      <Header OpenSidebar={OpenSidebar}/>
+      <Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/>
+      <Home />
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+
+export default App
