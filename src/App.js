c1ec8a8e57eece2d4302ef88cb145e92ad0c4c9d
hirsuitedevil <adhruv856@gmail.com>
2024-07-10 09:30:29 +0530

    added Homepage

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Signup from "./pages/SignUp";
 import Signin from "./pages/SignIn";
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Homepage />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/dashboard" element={<Dashboard />} />
